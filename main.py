7cc3148ad70a671db708f0620fe2b35c97d34637
esobirjon2197 <esobirjon2197@gmail.com>
2025-12-12 16:06:56 +0500

    Add power calculations for various numbers

---
+++ main.py
@@ -0,0 +1,11 @@
+# 2-m
+import math
+
+print(math.pow(5,3))
+print(math.pow(2,10))
+print(math.pow(7,2))
+print(math.pow(10,4))
+print(math.pow(9,3))
+print(math.pow(4,5))
+print(math.pow(3, 6))
+print(math.pow(2,5))
